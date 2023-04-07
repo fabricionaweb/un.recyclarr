c0e72318f0c715162c03e9278ee7bfb2746c4f19
Fabricio Silva <hi@fabricio.dev>
2023-04-07 01:06:05 +0100

    Add build frontend to makepkg script

--- makepkg.sh
+++ makepkg.sh
@@ -1,26 +1,41 @@
-#!/bin/bash -e
+#!/bin/bash -ex
+
+version="4.3.0"
+
+rootfs=package/rootfs
+emhttp=$rootfs/usr/local/emhttp/plugins/un.recyclarr
+bin=$rootfs/usr/local/bin
 
 prepare() {
   # delete previous stuff
   rm -rf package
   # create rootfs
-  mkdir -p package/rootfs/usr/local/emhttp/plugins/un.recyclarr package/rootfs/usr/local/bin
+  mkdir -p $emhttp $bin
 }
 
 download() {
-  # download latest always
-  wget -qO- https://github.com/recyclarr/recyclarr/releases/latest/download/recyclarr-linux-x64.tar.xz | tar xvJ -C package/rootfs/usr/local/bin
+  # download and extract
+  wget -qO- --show-progress https://github.com/recyclarr/recyclarr/releases/download/v$version/recyclarr-linux-x64.tar.xz | tar xvJ -C $bin
+}
+
+frontend_build() {
+  # assuming you have node, install deps
+  npm ci
+  # build
+  npm run build -- --minify
 }
 
 copy() {
   # copy emhttp plugin
-  cp -av source/* package/rootfs/usr/local/emhttp/plugins/un.recyclarr
+  cp -av source/* $emhttp
+  # remove frontend sources
+  rm -rf $emhttp/assets/{entry.js,modules}
 }
 
 compress() {
   # change workdir to rootfs
-  cd package/rootfs
-  # create the package (bsdtar)
+  cd $rootfs
+  # create the package (bsdtar/mac)
   tar -cvJ --format gnutar --numeric-owner --uid 0 --gid 0 -f ../un.recyclarr.txz .
   # restore workdir
   cd ../../
@@ -37,11 +52,12 @@ checksum() {
 
 clean() {
   # delete rootfs
-  rm -rf package/rootfs
+  rm -rf $rootfs
 }
 
 prepare
 download
+frontend_build
 copy
 compress
 checksum

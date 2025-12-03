7dec8d5a58cbb5697e5aa624ba4b925dbb2e327d
Fabricio Silva <hi@fabricio.dev>
2025-12-03 20:28:21 +0000

    fix #1 apply unraid >7 ui responsive changes

--- source/include/Helpers.php
+++ source/include/Helpers.php
@@ -3,5 +3,10 @@
 // Returns config files markup
 function mk_config_file($name, $fileName, $class = null) {
   $img = autov("/plugins/un.recyclarr/assets/yml-ico.svg", true);
-  return "<div class='flex user-list'><a href='?name=$fileName' data-file-name='$fileName' class='localURL yml-edit $class'><img src='$img' />$name</a></div>";
+  return "<div class='Panel'>
+    <a href='?name=$fileName' data-file-name='$fileName' class='localURL yml-edit $class'>
+      <span><img src='$img' /></span>
+      <div class='PanelText'>$name</div>
+    </a>
+  </div>";
 }
